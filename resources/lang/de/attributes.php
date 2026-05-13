c182ebddf01d25c8aa17cc5becd136f0c50778a0
Misaf <misaf.1990@gmail.com>
2026-05-13 13:06:05 +0330

    feat: add German translations

---
+++ resources/lang/de/attributes.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'action'     => 'Aktion',
+    'ip_address' => 'IP-Adresse',
+    'ip_country' => 'IP-Land',
+    'user_agent' => 'User-Agent',
+    'created_at' => 'Erstellt am',
+    'updated_at' => 'Aktualisiert am',
+];
