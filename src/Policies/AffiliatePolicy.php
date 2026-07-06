1c6fcae5985c3c6323b866537e4209bbec7ada7b
Misaf <misaf.1990@gmail.com>
2026-07-06 13:06:29 +0330

    Clean up module autoload and policies

--- src/Policies/AffiliatePolicy.php
+++ src/Policies/AffiliatePolicy.php
@@ -5,114 +5,114 @@ declare(strict_types=1);
 namespace Misaf\VendraAffiliate\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraAffiliate\Models\Affiliate;
-use Misaf\VendraUser\Models\User;
 
 final class AffiliatePolicy
 {
     use HandlesAuthorization;
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @return bool
      */
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
         return $user->can('create-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @param Affiliate $affiliate
      * @return bool
      */
-    public function delete(User $user, Affiliate $affiliate): bool
+    public function delete(Authorizable $user, Affiliate $affiliate): bool
     {
         return $user->can('delete-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @return bool
      */
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
         return $user->can('delete-any-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @param Affiliate $affiliate
      * @return bool
      */
-    public function forceDelete(User $user, Affiliate $affiliate): bool
+    public function forceDelete(Authorizable $user, Affiliate $affiliate): bool
     {
         return $user->can('force-delete-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @return bool
      */
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
         return $user->can('force-delete-any-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @param Affiliate $affiliate
      * @return bool
      */
-    public function replicate(User $user, Affiliate $affiliate): bool
+    public function replicate(Authorizable $user, Affiliate $affiliate): bool
     {
         return $user->can('replicate-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @param Affiliate $affiliate
      * @return bool
      */
-    public function restore(User $user, Affiliate $affiliate): bool
+    public function restore(Authorizable $user, Affiliate $affiliate): bool
     {
         return $user->can('restore-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @return bool
      */
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
         return $user->can('restore-any-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @param Affiliate $affiliate
      * @return bool
      */
-    public function update(User $user, Affiliate $affiliate): bool
+    public function update(Authorizable $user, Affiliate $affiliate): bool
     {
         return $user->can('update-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @param Affiliate $affiliate
      * @return bool
      */
-    public function view(User $user, Affiliate $affiliate): bool
+    public function view(Authorizable $user, Affiliate $affiliate): bool
     {
         return $user->can('view-affiliate');
     }
 
     /**
-     * @param User $user
+     * @param Authorizable $user
      * @return bool
      */
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
         return $user->can('view-any-affiliate');
     }
