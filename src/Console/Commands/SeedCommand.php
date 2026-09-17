46350fe6366cc3413356a4e8fe29aae25e9ed3b4
misaf <misaf.1990@gmail.com>
2026-09-17 19:48:56 +0000

    test(store): cover the storefront:lifecycle command

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace Misaf\VendraAffiliate\Console\Commands;
 
 use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
 use Misaf\VendraAffiliate\AffiliatePlugin;
 use Misaf\VendraAffiliate\Database\Seeders\DemoContentSeeder;
 use Misaf\VendraAffiliate\Database\Seeders\PermissionPolicySeeder;
 use Misaf\VendraSupport\Tenancy\Console\Commands\TenantSeedCommand;
 
 #[Description('Seed affiliate module data for a tenant')]
+#[Signature(self::MODULE_NAME.':seed
+        {tenant? : Tenant ID or slug to seed affiliate data for}
+        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}')]
 final class SeedCommand extends TenantSeedCommand
 {
     protected const string MODULE_NAME = AffiliatePlugin::ID;
-
-    protected $signature = self::MODULE_NAME.':seed
-        {tenant? : Tenant ID or slug to seed affiliate data for}
-        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}';
 
     /**
      * @return array<string, class-string>
